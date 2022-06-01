89bb4b581c180e61998658b1eb631c38cc724f0a
Nick White <bbycinka@gmail.com>
2022-06-01 12:25:21 +0300

    feat: создал блок wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import {Promo} from "./components/Promo";
+import {Wrapper} from "./components/Wrapper";
 
 function App() {
   return (
     <div className="App">
         <Promo>
+            <Wrapper>
+
+            </Wrapper>
         </Promo>
     </div>
   );
