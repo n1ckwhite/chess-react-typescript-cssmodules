0e1444aa5dc5daa94f09a537aa0fcbe2b3ff043a
Nick White <bbycinka@gmail.com>
2022-06-02 17:29:19 +0300

    build v2

---
+++ build/precache-manifest.c07c79095f116a1d4a9c14ab1b418e28.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d7acb1ce2c65b30993d",
+    "url": "/static/css/main.5a91d2ad.chunk.css"
+  },
+  {
+    "revision": "3d7acb1ce2c65b30993d",
+    "url": "/static/js/main.48d2f84d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9283bec22450ab132fe9",
+    "url": "/static/js/2.bde16d8d.chunk.js"
+  },
+  {
+    "revision": "ab809a60d5001dbcb9ef44ba34dc4ef0",
+    "url": "/static/media/logo.ab809a60.svg"
+  },
+  {
+    "revision": "fa38483fe1360797def6f645af3e3d7f",
+    "url": "/static/media/bools.fa38483f.svg"
+  },
+  {
+    "revision": "7389180f453af372099755dca6813946",
+    "url": "/static/media/arrow.7389180f.svg"
+  },
+  {
+    "revision": "463b46e54291bd5a041c874973f34212",
+    "url": "/static/media/desk.463b46e5.svg"
+  },
+  {
+    "revision": "74a3fe12babceb62e52d97ac015a6044",
+    "url": "/static/media/andrey.74a3fe12.png"
+  },
+  {
+    "revision": "63f9db617a99aa624756dec728bfc190",
+    "url": "/static/media/nikita.63f9db61.png"
+  },
+  {
+    "revision": "7469863503c34432ac83b27f44e29f5b",
+    "url": "/static/media/aleksey.74698635.png"
+  },
+  {
+    "revision": "301eebd4d91b349e8ca42936de869d74",
+    "url": "/static/media/bool.301eebd4.svg"
+  },
+  {
+    "revision": "77ebb81a1f7e296ba70623f9fed86335",
+    "url": "/static/media/bools_green.77ebb81a.svg"
+  },
+  {
+    "revision": "c25d469f9b12cbe7020a9f716d815706",
+    "url": "/static/media/battle1.c25d469f.svg"
+  },
+  {
+    "revision": "467f3c6a35acbc1884de20fee512f225",
+    "url": "/static/media/battle2.467f3c6a.png"
+  },
+  {
+    "revision": "ce7f087df97fa632972ce915c5f25570",
+    "url": "/static/media/battle3.ce7f087d.png"
+  },
+  {
+    "revision": "70bc84e8e03a2d387a2b3bde06e656f8",
+    "url": "/static/media/desk_green.70bc84e8.svg"
+  },
+  {
+    "revision": "f3515833be8da02059846ed96fbd2910",
+    "url": "/index.html"
+  }
+];
